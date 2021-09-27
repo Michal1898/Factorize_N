7cef3b1b99e79f220edb490e0c1a697c8f86474f
Michal1898 <michal.volf@gmail.com>
2021-09-27 22:36:51 +0200

    prime numbers error fixed.Now is more efficient.

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 def factorize(n):
     prime_numbers = [2]
     for tested_number in range(3, n + 1):
+        is_prime = True
         for divisor in prime_numbers:
-            is_prime = True
             if tested_number % divisor == 0:
                 is_prime = False
 
         if is_prime:
             prime_numbers.append(tested_number)
 
+    #print(prime_numbers)
     dividend = n
     divisors = []
-    #number_decomposition = f"{n} ="
-    number_decomposition = "%s =" %(n)
+    # number_decomposition = f"{n} ="
+    number_decomposition = "%s =" % (n)
     while dividend > 1:
         for prime in list(filter(lambda x: x <= dividend, prime_numbers)):
             if dividend % prime == 0:
@@ -26,8 +27,9 @@ def factorize(n):
     number_decomposition = number_decomposition[:-1]
     print(number_decomposition)
 
+
 if __name__ == "__main__":
-    factorize(360)
+    factorize(100000)
     factorize(19)
     factorize(75)
     factorize(1001)
